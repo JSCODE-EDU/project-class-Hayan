1b7bec48cc1b3f1da06cc6e959c0afd863364421
[Hayan] <[c_white723@naver.com]>
2023-05-11 21:19:30 +0900

    feat: 생성, 수정 메서드 추가

--- src/main/java/hayan/board/domain/Board.java
+++ src/main/java/hayan/board/domain/Board.java
@@ -21,16 +21,32 @@ public class Board {
     private String name;
     private String password;
     private String title;
+    @Column(columnDefinition = "text")
     private String content;
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @Builder
-    public Board(String name, String password, String title, String content) {
+    public Board(String name, String password, String title, String content, LocalDateTime createdAt) {
         this.name = name;
         this.password = password;
         this.title = title;
         this.content = content;
-        createdAt = LocalDateTime.now();
+        this.createdAt = createdAt;
+    }
+
+    public static Board createBoard(String name, String password, String title, String content) {
+        return Board.builder()
+                .name(name)
+                .password(password)
+                .title(title)
+                .content(content)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
